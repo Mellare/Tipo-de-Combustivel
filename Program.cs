2c8dac9f7124dadc26ea4bce7ed5d88c75cd9d7e
Mellare <86076128+Mellare@users.noreply.github.com>
2023-03-27 23:15:24 -0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,35 @@
+int x = 0;// Álcool
+int y = 0;//Gasolina
+int z = 0;//Diesel
+
+bool entraNoLaco = true;
+
+while(entraNoLaco)
+{
+
+int codigo = int.Parse(Console.ReadLine());
+
+
+if(codigo == 1)
+{
+    x = x + 1;
+}
+
+if(codigo == 2)
+{
+    y = y + 1;
+}
+
+if(codigo == 3){
+    z = z + 1;
+}
+
+if(codigo == 4){
+    entraNoLaco = false;
+}
+
+}
+Console.WriteLine("MUITO OBRIGADO");
+Console.WriteLine("Alcool: " + x);
+Console.WriteLine("Gasolina: " + y);
+Console.WriteLine("Diesel: " + z);
